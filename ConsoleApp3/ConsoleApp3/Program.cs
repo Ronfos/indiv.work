44c59be15681cb2384de8bbe5cd296e412ec6334
Ronfos <74715764+Ronfos@users.noreply.github.com>
2023-11-08 19:22:00 +0200

    Add files via upload

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Sqr(double x)
+        {
+            Console.WriteLine($"Число {x} в квадрате = {Math.Pow(x, 2)}");
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число для возведения в квадрат: ");
+            double a = double.Parse(Console.ReadLine());
+            Sqr(a);
+            Console.ReadKey();
+        }
+    }
+}
